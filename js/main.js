1fdc1dbea09cb252bcd1993d92ebcad57a6fccb6
wushiming <wushiming1027@gmail.com>
2016-04-09 16:42:03 +0800

    TESTED WITH THE PROGRESS BAR, CAN READ "GEK1505" as a module to update the progress

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var hypothesis = "Sample hypothesis";				//Enter hypothesis here
+var independentVariables = ["IV1", "IV2", "IV3"];	//Enter IVs here
+var dependentVariables = ["DV1", "DV2", "DV3"];		//Enter DVs here
+var randomVariables = ["RV1", "RV2"];				//Enter RVs here
+var controlVariables = ["CV1", "CV2"];				//Enter CVs here
+var experimentalConditions = ["IV1DV1", "IV1DV2", "IV1DV3", "IV2DV1", "IV2DV2", "IV2DV3", "IV3DV1", "IV3DV2", "IV3DV3"];
+var designTypes = [["between_subject", "No_of_participants"], ["within_subject", "No_of_participants"]];	//Enter the number of participants required for each design
+
+//Enter the D3 code below
